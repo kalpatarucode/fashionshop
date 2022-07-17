847a2d54a50d63081fb3dbca1ac38157e3614491
Kalptaru Code <vananishaktisir@gmail.com>
2022-07-17 00:20:00 -0700

    change login page

--- admin/loader.php
+++ admin/loader.php
@@ -1,7 +1,7 @@
 <!-- Page Loader -->
 <div class="page-loader-wrapper">
     <div class="loader">
-        <div class="m-t-30"><img src="assets/images/logo.svg" width="48" height="48" alt="Alpino"></div>
+        <div class="m-t-30"><img src="/fashionshop/admin/assets/images/logo/logo.png" width="48" height="48" alt="fashionshop"></div>
         <p>Please wait...</p>        
     </div>
 </div>
